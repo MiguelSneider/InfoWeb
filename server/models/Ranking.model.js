a204e3b82d4d100c2ede342bb3635af777e775c8
Esneider Ardila <mgd.26@hotmail.com>
2022-12-26 11:31:54 -0500

    creacion BD en railway para despliegue

--- server/models/Ranking.model.js
+++ server/models/Ranking.model.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 
 mongoose
     //.connect('mongodb://127.0.0.1:27017/languages', function (error){
-    .connect('mongodb://mongo:BbX95EiNqJljgWEC9HNn@containers-us-west-186.railway.app:6493', function (error){
+    .connect('mongodb://mongo:rzA48YS56cejNP4KYVDo@containers-us-west-108.railway.app:7908', function (error){
         if (error) throw error;
         console.log('DATABASE CONNECTION');
     });
@@ -20,7 +20,7 @@ mongoose
 
     })
     
-    //const Ranking = mongoose.model('ranking', coasterSchema)
-    const Ranking = mongoose.model('Model', coasterSchema, 'ranking')
+    const Ranking = mongoose.model('ranking', coasterSchema)
+    //const Ranking = mongoose.model('Model', coasterSchema, 'ranking')
 
     module.exports = Ranking
