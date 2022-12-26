a74d3321bd6ef406ca285bd472dcb3852ba1ca14
Esneider Ardila <mgd.26@hotmail.com>
2022-12-26 15:29:02 -0500

    configuraciones en la conexion a la BD

--- server/models/Ranking.model.js
+++ server/models/Ranking.model.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 
 mongoose
     //.connect('mongodb://127.0.0.1:27017/languages', function (error){
-    .connect('mongodb://mongo:rzA48YS56cejNP4KYVDo@containers-us-west-108.railway.app:7908', function (error){
+    .connect('mongodb://mongo:0MEoCEpiJZE6W93DbdhP@containers-us-west-80.railway.app:6233', function (error){
         if (error) throw error;
         console.log('DATABASE CONNECTION');
     });
@@ -20,7 +20,7 @@ mongoose
 
     })
     
-    const Ranking = mongoose.model('ranking', coasterSchema)
-    //const Ranking = mongoose.model('Model', coasterSchema, 'ranking')
+    //const Ranking = mongoose.model('ranking', coasterSchema)
+    const Ranking = mongoose.model('Model', coasterSchema, 'ranking')
 
     module.exports = Ranking
