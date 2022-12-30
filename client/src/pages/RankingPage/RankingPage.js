260d4b8276f84456351dad2b48059bc0362040eb
Esneider Ardila <mgd.26@hotmail.com>
2022-12-30 01:47:03 -0500

    archivo despliegue para explicacion del deploy

--- client/src/pages/RankingPage/RankingPage.js
+++ client/src/pages/RankingPage/RankingPage.js
@@ -9,7 +9,7 @@ const RankingPage = () => {
 
     const loadRanking = () => {
         //fetch('http://localhost:5005/api/ranking')
-         fetch('https://infoweb-server-production.up.railway.app/api/ranking')
+        fetch('https://infoweb-server-production.up.railway.app/api/ranking')
         .then(res => res.json())
         .then(allRanking => setRanking(allRanking))
         //.then(allRanking => console.log(allRanking))
